f77e472be65788f465712122c0d1da981c3c6c68
Victor <victor.wisphub@gmail.com>
2020-08-06 15:57:18 -0500

    Fix: cascade model

--- notifications/models.py
+++ notifications/models.py
@@ -1,9 +1,17 @@
 from swapper import swappable_setting
 
 from .base.models import AbstractNotification, notify_handler  # noqa
-
+from django.db import models
+from django.conf import settings
 
 class Notification(AbstractNotification):
+    recipient = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        blank=True,
+        null=True,
+        related_name='notifications',
+        on_delete=models.SET_NULL
+        )
 
     class Meta(AbstractNotification.Meta):
         abstract = False
